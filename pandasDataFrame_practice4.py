5f9f2ba31ef6a2894f65a85d9eb2deccc1e8c386
itachi_hitachi <yusenng88@gmail.com>
2022-12-02 23:03:13 +0800

    Chore:Finished with fourth tutorial

---
+++ pandasDataFrame_practice4.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+exam_data = {
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19]
+}
+
+
+exam = pd.DataFrame(exam_data, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']) #creating your own creating index
+
+print(exam.loc["a","b","c"]) #using a list of indexes, since your indexes is changed you have to change as well.
+print(exam.iloc[:3]) #using purely just integer
